9ae6192183f5787bcac5458749a846e1018b53c2
Zhifeng Shi <zhifeng.shi@betprophet.co>
2022-12-08 15:34:44 -0500

    remove comment

--- src/main.py
+++ src/main.py
@@ -5,8 +5,6 @@ from src.log import logging
 if __name__ == '__main__':
     logging.info("testing MM api")
     mm_instance = mm_calls.MMInteractions()
-    # mm_instance.login()
-    # mm_keys = mm_instance.get_key()
     mm_instance.mm_login()
     mm_instance.get_balance()
     mm_instance.seeding()
