bafe3093a07854f893ff242aa5a8f8c9eeaaeb1d
Zhifeng Shi <zhangfeng3366@gmail.com>
2024-06-11 10:48:54 -0400

    do not cancel wagers

--- src/mm_calls.py
+++ src/mm_calls.py
@@ -405,7 +405,7 @@ class MMInteractions:
         logging.info("schedule to bet every 10 seconds")
         schedule.every(20).seconds.do(self.start_betting)
         # schedule.every(9).seconds.do(self.random_cancel_wager)
-        schedule.every(7).seconds.do(self.random_batch_cancel_wagers)
+        # schedule.every(7).seconds.do(self.random_batch_cancel_wagers)
         schedule.every(8).minutes.do(self.__auto_extend_session)
         schedule.every(200).seconds.do(self.seeding)
 
