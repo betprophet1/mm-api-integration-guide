f411bbc0ddc93fda479273730983186455f333ff
Zhifeng Shi <zhifeng.shi@Zhifengs-MacBook-Pro-2.local>
2025-08-12 09:02:35 -0400

    fix

--- src/mm_calls.py
+++ src/mm_calls.py
@@ -318,7 +318,7 @@ class MMInteractions:
                                             'external_id': external_id,
                                             'line_id': selection[picked_selection]['line_id'],
                                             'odds': odds_to_bet,
-                                            'stake': 100.0,
+                                            'stake': 10.0,
                                             'delayed_return': True,
                                         }
                                         try:
@@ -333,13 +333,13 @@ class MMInteractions:
                                             logging.info("successfully")
                                             self.wagers[external_id] = json.loads(bet_response.content).get('data', {})['wager']['id']
                                         # testing batch place wagers
-                                        '''batch_n = 10
+                                        batch_n = 7
                                         external_id_batch = [str(uuid.uuid1()) for x in range(batch_n)]
                                         batch_body_to_send = [{
                                             'external_id': external_id_batch[x],
                                             'line_id': selection[0]['line_id'],
                                             'odds': odds_to_bet,
-                                            'stake': 100.0
+                                            'stake': 10.0
                                         } for x in range(batch_n)]
                                         batch_bet_response = requests.post(batch_bet_url, json={"data": batch_body_to_send},
                                                                            headers=self.__get_auth_header())
@@ -357,7 +357,7 @@ class MMInteractions:
                                         batch_cancel_body = [{'wager_id': self.wagers[x],
                                                               'external_id': x} for x in batch_keys_to_cancel]
                                         batch_cancel_url = urljoin(self.base_url, config.URL['mm_batch_cancel'])
-                                        try:
+                                        '''try:
                                             response = requests.post(batch_cancel_url, json={'data': batch_cancel_body},
                                                                      headers=self.__get_auth_header())
                                         except Exception as e:
@@ -444,7 +444,7 @@ class MMInteractions:
         logging.info("schedule to bet every 10 seconds")
         schedule.every(8).seconds.do(self.start_betting)
         # schedule.every(9).seconds.do(self.random_cancel_wager)
-        # schedule.every(7).seconds.do(self.random_batch_cancel_wagers)
+        schedule.every(7).seconds.do(self.random_batch_cancel_wagers)
         schedule.every(8).minutes.do(self.__auto_extend_session)
         schedule.every(30).minutes.do(self.seeding)
 
